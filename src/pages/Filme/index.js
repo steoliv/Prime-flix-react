f5cfeac2b0b388bc068475e9c8ef638711f2501f
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-08-15 10:35:35 -0300

    Ajustando pagina de detalhe de cada filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../../services/api";
+
+
 function Filme(){
+    const {id} = useParams();
+
+    useEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "41cc98c8327a81134f528d684b42ea6e",
+                    language: "pt-BR",
+                }
+            })
+        }
+        loadFilme();
+    })
     return(
         <div>
-            <h1> Filmes</h1>
+            <h1> Acessando filme {id}</h1>
         </div>
     )
 }
